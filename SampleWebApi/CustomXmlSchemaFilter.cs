4777a45009ea98c52685ac821fa0342cc8f6a988
Peeush Agarwal <pa@euroform.com>
2019-10-16 15:03:41 +0530

    Move CustomXmlSchemaFilter out to own file.

---
+++ SampleWebApi/CustomXmlSchemaFilter.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SampleWebApi
+{
+    public class CustomXmlSchemaFilter : ISchemaFilter
+    {
+        private const string _SCHEMA_ARRAY_TYPE = "array";
+        private const string _SCHEMA_STRING_TYPE = "string";
+        private const string _PREFIX_ARRAY = "ArrayOf";
+
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (context.ApiModel.Type.IsValueType)
+                return;
+
+            if (schema.Type == _SCHEMA_STRING_TYPE)
+                return;
+
+            schema.Xml = new OpenApiXml
+            {
+                Name = context.ApiModel.Type.Name
+            };
+
+            if (schema.Type == _SCHEMA_ARRAY_TYPE)
+            {
+                var itemName = string.Empty;
+                if (schema.Items.Reference != null)
+                {
+                    itemName = schema.Items.Reference.Id;
+                }
+                else
+                {
+                    schema.Items.Xml = new OpenApiXml
+                    {
+                        Name = schema.Items.Type,
+                    };
+                    itemName = schema.Items.Type;
+                }
+
+                schema.Xml = new OpenApiXml
+                {
+                    Name = $"{_PREFIX_ARRAY}{itemName}",
+                    Wrapped = true,
+                };
+            }
+
+            if (schema.Properties == null)
+            {
+                return;
+            }
+
+            foreach (var property in schema.Properties.Where(x => x.Value.Type == _SCHEMA_ARRAY_TYPE))
+            {
+                property.Value.Items.Xml = new OpenApiXml
+                {
+                    Name = property.Value.Items.Type,
+                };
+                property.Value.Xml = new OpenApiXml
+                {
+                    Name = property.Key,
+                    Wrapped = true
+                };
+            }
+        }
+    }
+}
--- SampleWebApi/Startup.cs
+++ SampleWebApi/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -16,7 +15,6 @@ using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using SampleWebApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace SampleWebApi
 {
@@ -87,68 +85,6 @@ namespace SampleWebApi
             });
 
             app.UseMvc();
-        }
-    }
-
-    public class CustomXmlSchemaFilter : ISchemaFilter
-    {
-        private const string _SCHEMA_ARRAY_TYPE = "array";
-        private const string _SCHEMA_STRING_TYPE = "string";
-        private const string _PREFIX_ARRAY = "ArrayOf";
-
-        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
-        {
-            if (context.ApiModel.Type.IsValueType)
-                return;
-
-            if (schema.Type == _SCHEMA_STRING_TYPE)
-                return;
-
-            schema.Xml = new OpenApiXml
-            {
-                Name = context.ApiModel.Type.Name
-            };
-
-            if (schema.Type == _SCHEMA_ARRAY_TYPE)
-            {
-                var itemName = string.Empty;
-                if (schema.Items.Reference != null)
-                {
-                    itemName = schema.Items.Reference.Id;
-                }
-                else
-                {
-                    schema.Items.Xml = new OpenApiXml
-                    {
-                        Name = schema.Items.Type,
-                    };
-                    itemName = schema.Items.Type;
-                }
-
-                schema.Xml = new OpenApiXml
-                {
-                    Name = $"{_PREFIX_ARRAY}{itemName}",
-                    Wrapped = true,
-                };
-            }
-
-            if (schema.Properties == null)
-            {
-                return;
-            }
-
-            foreach (var property in schema.Properties.Where(x => x.Value.Type == _SCHEMA_ARRAY_TYPE))
-            {
-                property.Value.Items.Xml = new OpenApiXml
-                {
-                    Name = property.Value.Items.Type,
-                };
-                property.Value.Xml = new OpenApiXml
-                {
-                    Name = property.Key,
-                    Wrapped = true
-                };
-            }
         }
     }
 }
